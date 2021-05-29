31442c391f511d186fa4616dff2aeb850573be8f
philripd <phillipanddang@gmail.com>
2021-05-29 00:41:29 -0700

    Fixed IllegalStateException warning, replaced scroll listener with button
    Additional Changes:
    - Button background tint darkened to increase readability
    - Prepared checkboxes for search filter implementation

--- app/src/main/java/com/codepath/apps/destination_vacation/fragments/SearchFragment.java
+++ app/src/main/java/com/codepath/apps/destination_vacation/fragments/SearchFragment.java
@@ -1,12 +1,11 @@
 package com.codepath.apps.destination_vacation.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,11 +17,9 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Checkable;
 import android.widget.EditText;
 
 import com.codepath.apps.destination_vacation.BuildConfig;
-import com.codepath.apps.destination_vacation.EndlessRecyclerViewScrollListener;
 import com.codepath.apps.destination_vacation.R;
 import com.codepath.apps.destination_vacation.adapters.DestinationAdapter;
 import com.codepath.apps.destination_vacation.models.Destination;
@@ -31,14 +28,11 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
-
-import static androidx.core.content.ContextCompat.getSystemService;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -51,16 +45,18 @@ public class SearchFragment extends Fragment {
     public static final String URL = "https://api.opentripmap.com/0.1/en/places/bbox?lon_min=-120&lat_min=25&lon_max=0&lat_max=50&kinds=interesting_places&format=json&apikey=" + BuildConfig.OPENTRIPMAP_API_KEY;
 
     private EditText etSearch;
-    private Button btnSearch;
-    private RecyclerView rvLocations;
-
+    private CheckBox checkBox;
+    private CheckBox checkBox2;
     private CheckBox checkBox3;
-
-    EndlessRecyclerViewScrollListener scrollListener;
+    private CheckBox checkBox4;
+    private Button btnSearch;
+    private RecyclerView rvDestinations;
+    private Button btnLoadMore;
 
     private JSONArray results;
     private final int limit = 10;
     private int destinationIndex = 0;
+    private int page;
 
     List<Destination> destinations;
 
@@ -77,53 +73,77 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         etSearch = view.findViewById(R.id.etSearch);
+        checkBox = view.findViewById(R.id.checkBox);
+        checkBox2 = view.findViewById(R.id.checkBox2);
+        checkBox3 = view.findViewById(R.id.checkBox3);
+        checkBox4 = view.findViewById(R.id.checkBox4);
         btnSearch = view.findViewById(R.id.btnSearch);
-        rvLocations = view.findViewById(R.id.rvDestinations);
+        rvDestinations = view.findViewById(R.id.rvDestinations);
+        btnLoadMore = view.findViewById(R.id.btnLoadMore);
         destinations = new ArrayList<>();
 
-        checkBox3 = view.findViewById(R.id.checkBox3);
+        // Hide load more button
+        btnLoadMore.setVisibility(View.GONE);
 
         // Create the adapter
         final DestinationAdapter destinationAdapter = new DestinationAdapter(getContext(), destinations);
 
         // Set the adapter on the recycler view
-        rvLocations.setAdapter(destinationAdapter);
+        rvDestinations.setAdapter(destinationAdapter);
 
         // Set a Layout Manager on the recycler view
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
-        rvLocations.setLayoutManager(layoutManager);
+        rvDestinations.setLayoutManager(layoutManager);
 
-        rvLocations.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        // Add a divider to the recycler view
+        rvDestinations.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
 
         final AsyncHttpClient client = new AsyncHttpClient();
 
-        // Pagination
-        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+        // Set an InputMethodManager to hide the keyboard when the search button is pressed
+        InputMethodManager imm = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        // Load more results button listener
+        btnLoadMore.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                Log.i(TAG, "onLoadMore : " + page);
+            public void onClick(View v) {
+                // Lower/close keyboard
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+                // Calculate page number
+                page = destinationIndex / limit + 1;
+                Log.i(TAG, "Load more button pressed. Getting page " + page + " of results.");
 
                 try {
+                    // Add destination objects to the output ArrayList
                     destinations.addAll(Destination.fromJsonArray(results, limit, destinationIndex));
                     destinationIndex += limit;
-                    destinationAdapter.notifyDataSetChanged();
-                    Log.i(TAG, "Current items in recycler view: " + destinations.size());
+
+                    // Update recycler view
+                    rvDestinations.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            destinationAdapter.notifyItemInserted(destinations.size() - 1);
+                        }
+                    });
+
+                    Log.i(TAG, "Number of places in recycler view: " + destinations.size());
+
+                    // Show load more button if there are places not yet displayed
+                    if ((destinationIndex + limit) > results.length())
+                        btnLoadMore.setVisibility(View.GONE);
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit json exception", e);
                 }
             }
-        };
-        // Adds the scroll listener to RecyclerView
-        rvLocations.addOnScrollListener(scrollListener);
+        });
 
+        // Search button listener
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 // Lower/close keyboard
-                InputMethodManager imm = (InputMethodManager)getContext().getSystemService(getContext().INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-
 
                 Log.i(TAG, "Search button pressed. Query: " + etSearch.getText().toString());
 
@@ -133,16 +153,20 @@ public class SearchFragment extends Fragment {
                         Log.d(TAG, "onSuccess");
                         try {
                             results = json.jsonArray;
+                            Log.i(TAG, "Getting page 1 of results");
                             Log.i(TAG, "Results: " + results.toString());
 
+                            // Add destination objects to the output ArrayList
                             destinations.addAll(Destination.fromJsonArray(results, limit, destinationIndex));
                             destinationIndex += limit;
 
                             destinationAdapter.notifyDataSetChanged();
-                            Log.i(TAG, "Number of results: " + 500); // Currently, the API request gets the default 500 destinations
-                            Log.i(TAG, "Current items in recycler view: " + destinations.size());
-                            // Log.i(TAG, "Number of results: " + destinations.size());
-                            // Log.i(TAG, "10th item's xid: " + results.getJSONObject(l-1).getString("xid"));
+                            Log.i(TAG, "Number of places: " + results.length());
+                            Log.i(TAG, "Number of places in recycler view: " + destinations.size());
+
+                            // Show load more button if there are places not yet displayed
+                            if ((destinationIndex + limit) < results.length())
+                                btnLoadMore.setVisibility(View.VISIBLE);
                         } catch(JSONException e) {
                             Log.e(TAG, "Hit json exception", e);
                         }
